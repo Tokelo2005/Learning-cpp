0d82bc2b2f96f15ebcd32289f0dbf4f986627ac1
Tokelo2005 <122170034+Tokelo2005@users.noreply.github.com>
2026-07-06 18:29:15 +0200

    odd and even number game.cpp

---
+++ odd and even number game.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+   /*int number;
+   cout<<"Enter Number : ";
+   cin>>number;
+   
+   if (number%2==0){
+       cout<<number<<" is an even number";
+ }
+   
+   else {cout<<number<< " its a odd number";}
+       
+   // Task 2****************************
+   
+  /*  int mark;
+    int age;
+    string name;
+    cout<<"Enter Name of learner : ";
+    getline(cin,name);
+    cout<<"Enter age : ";
+    cin>>age;
+    cout<<"Enter leaner's mark : ";
+    cin>>mark;
+    
+    if(mark>=75 && mark<=100){ cout<<"Congrats you passed with a distiction";}
+    else if (mark>=50 && mark<=74){cout<<"Congrats you Passed";}
+    else if (mark >=0 && mark<=49){cout<<"Sorry but you Failed";}
+    else {cout<<"invalid Entry";}
+ 
+  
+    return 0;
+}
