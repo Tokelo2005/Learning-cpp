551ae097b403eac4e8d839a1228f73456b5a9616
Tokelo2005 <122170034+Tokelo2005@users.noreply.github.com>
2026-07-15 20:09:42 +0200

    Array intro.cpp

---
+++ Array intro.cpp
@@ -0,0 +1,14 @@
+//Learning array
+//code simply let's user input intergers then display them
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int array[4];
+for (int x=0 ; x<=3 ;x++){cin>> array[x];}
+  for(int y=0 ; y<=3; y++){cout<<array[y];}
+     return 0;
+}
